082f01c81a6a52393306f6614033f4daafa40c18
VIJAY YADAV <01shubhamupadhyay@gmail.com>
2024-03-17 21:45:56 +0530

    Started developement

---
+++ FareCalculator/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+class Station(models.Model):
+    name = models.CharField(max_length = 100)
+
+    def __str__(self) -> str:
+        return self.name
+
+class Fare(models.Model):
+    origin = models.ForeignKey(Station, on_delete=models.CASCADE, related_name='origin_fares')
+    destination = models.ForeignKey(Station, on_delete=models.CASCADE, related_name='origin_fares')
+    fare_amount = models.DecimalField(max_length=10, decimal_places=2)
