6091e5ff807d998d77201fa962a4bc72033e9f39
VIJAY YADAV <01shubhamupadhyay@gmail.com>
2024-03-18 19:36:13 +0530

    Added the namma metro map

--- FareCalculator/settings.py
+++ FareCalculator/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -116,7 +117,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+# Define the base URL for static files
+STATIC_URL = '/static/'
+
+# Specify the directories where Django will look for static files
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),  # Path to your static files directory
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
